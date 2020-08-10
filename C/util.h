98d42c9426854b8feb6fe33863dc3a61b2ca34f2
김명기 <33053284+riroan@users.noreply.github.com>
2020-08-10 22:59:42 +0900

    Add files via upload

---
+++ C/util.h
@@ -0,0 +1,114 @@
+#pragma once
+#define GOOD_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+#include <stdbool.h>
+
+typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE;
+void clrscr();
+void gotoxy(int x, int y);
+int wherex();
+int wherey();
+void setcursortype(CURSOR_TYPE c);
+void SetColor(int color);
+
+enum
+{
+	BLACK,
+	DARK_BLUE,
+	DARK_GREEN,
+	DARK_SKYBLUE,
+	DARK_RED,
+	DARK_VOILET,
+	DARK_YELLOW,
+	GRAY,
+	DARK_GRAY,
+	BLUE,
+	GREEN,
+	SKYBLUE,
+	RED,
+	VIOLET,
+	YELLOW,
+	WHITE
+};
+
+#define delay(n) Sleep(n)                              // n/1000초만큼 시간 지연
+#define randomize() srand((unsigned)time(NULL))         // 난수 발생기 초기화
+#define random(n) (rand() % (n))                        //0~n까지의 난수 발생
+
+// 이 매크로가 정의되어 있으면 함수의 원형만 선언하고 정의는 하지 않는다.
+#ifndef GOOD_PROTOTYPE_ONLY
+
+// 화면을 모두 지운다.
+void clrscr()
+{
+	system("cls");
+}
+
+// 커서를 x,y좌표로 이동시킨다.
+void gotoxy(int x, int y)
+{
+	COORD Cur;
+	Cur.X = x;
+	Cur.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+}
+
+// 커서의 x 좌표를 조사한다.
+int wherex()
+{
+	CONSOLE_SCREEN_BUFFER_INFO BufInfo;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &BufInfo);
+	return BufInfo.dwCursorPosition.X;
+}
+
+// 커서의 y좌표를 조사한다.
+int wherey()
+{
+	CONSOLE_SCREEN_BUFFER_INFO BufInfo;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &BufInfo);
+	return BufInfo.dwCursorPosition.Y;
+}
+
+// 커서를 숨기거나 다시 표시한다.
+void setcursortype(CURSOR_TYPE c)
+{
+	CONSOLE_CURSOR_INFO CurInfo;
+
+	switch (c) {
+	case NOCURSOR:
+		CurInfo.dwSize = 1;
+		CurInfo.bVisible = FALSE;
+		break;
+	case SOLIDCURSOR:
+		CurInfo.dwSize = 100;
+		CurInfo.bVisible = TRUE;
+		break;
+	case NORMALCURSOR:
+		CurInfo.dwSize = 20;
+		CurInfo.bVisible = TRUE;
+		break;
+	}
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CurInfo);
+}
+
+double rand2(const double min, const double max)		// min~max사이의 랜덤 실수값 반환
+{
+	double temp = (double)rand() / (double)RAND_MAX;
+	temp = min + (max - min)*temp;
+
+	return temp;
+}
+
+void SetColor(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+#endif  
